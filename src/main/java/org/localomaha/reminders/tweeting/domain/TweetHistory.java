e427ceabe7bd339798d352f2920fd28cdb44ba3d
Matt Payne <Payne@MattPayne.org>
2021-02-24 08:57:25 -0600

    I like initial values of 1

--- src/main/java/org/localomaha/reminders/tweeting/domain/TweetHistory.java
+++ src/main/java/org/localomaha/reminders/tweeting/domain/TweetHistory.java
@@ -21,7 +21,7 @@ public class TweetHistory {
             name = "primary_sequence",
             sequenceName = "primary_sequence",
             allocationSize = 1,
-            initialValue = 10000
+            initialValue = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
