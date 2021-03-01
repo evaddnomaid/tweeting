569101a64b31d9c280cef9b15f30f014aa00d962
Matt Payne <Payne@MattPayne.org>
2021-03-01 09:08:45 -0600

    Added lombok.

--- src/main/java/org/localomaha/reminders/tweeting/HomeController.java
+++ src/main/java/org/localomaha/reminders/tweeting/HomeController.java
@@ -1,6 +1,8 @@
 package org.localomaha.reminders.tweeting;
 
+import lombok.extern.log4j.Log4j2;
 import org.localomaha.reminders.tweeting.model.TweetsDTO;
+import org.localomaha.reminders.tweeting.service.TweetsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.validation.Valid;
 import java.util.Date;
 
-
+@Log4j2
 @Controller
 public class HomeController {
+
+    TweetsService tweetsService;
+
+    public HomeController(TweetsService ts) {
+        this.tweetsService = ts;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
@@ -23,16 +31,17 @@ public class HomeController {
     @GetMapping("/add")
     public String addGet(Model model) {
         TweetsDTO tweet = new TweetsDTO();
-        model.addAttribute("tweet",tweet);
+        model.addAttribute("tweet", tweet);
         setCurrentDate(model);
         return "add";
     }
 
     @PostMapping("/add")
     public String addPost(Model model, TweetsDTO tweet, BindingResult bindingResult) {
-        System.out.println(tweet);
         setCurrentDate(model);
-        model.addAttribute("tweet",tweet);
+        model.addAttribute("tweet", tweet);
+        Long id = tweetsService.create(tweet);
+        log.info("Created tweet id={}",id);
         return "add";
     }
 
